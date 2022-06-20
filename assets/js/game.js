6c410070def2354e67cd8c9c821e141ef7d3f940
Demi Hayashi <demi.h@me.com>
2022-06-20 12:49:31 -0700

    fixed skip function

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,10 +21,18 @@ for(var i = 0; i < enemyNames.length; i++) {
 }
 
 var fight = function(enemyName) {
-    while(enemyHealth > 0) {
+    while(playerHealth > 0 && enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-        if (promptFight === "fight" || promptFight === "FIGHT") {
+        if (promptFight === "skip" || promptFight === "SKIP") {
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                playerMoney = playerMoney - 10;
+                console.log("playerMoney", playerMoney);
+                break;
+            }
+        }
 
     // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
     enemyHealth = enemyHealth - playerAttack;
@@ -33,8 +41,9 @@ var fight = function(enemyName) {
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
-    }
-    else {
+        playerMoney = playerMoney + 20;
+        break;
+    } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
     // Subtract the value og 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
@@ -47,24 +56,10 @@ var fight = function(enemyName) {
     if (playerHealth <= 0) {
         window.alert(playerName + " has died!");
         break;
-    }
-    else {
+    } else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
-
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
-    var confirmSkip = window.confirm("Are you sure you'd like to skip?");
-    if (confirmSkip) {
-        window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        playerMoney = playerMoney - 2;
-    }
-    else {
-        fight();
-    }
-    } else {
-    window.alert("You need to choose a valid option. Try again!");
-    }
-    }
+}
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
@@ -73,4 +68,4 @@ for (var i = 0; i < enemyNames.length; i++) {
     fight(pickedEnemyName);
 }
 
-//fight();
+// fight();
